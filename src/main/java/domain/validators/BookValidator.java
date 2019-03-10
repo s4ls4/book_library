d3b25eca5cf241262a884a4f81d65d8f9339453c
Muradasil Birhan <b98lion@gmail.com>
2019-03-10 15:27:00 +0200

    Start test

--- src/main/java/domain/validators/BookValidator.java
+++ src/main/java/domain/validators/BookValidator.java
@@ -12,19 +12,16 @@ public class BookValidator implements Validator<Book> {
         if(book.getName() == null) {
             throw new ValidatorException("name must not be null");
         }
-        if(!book.getName().matches("[a-zA-Z]+")) {
-            throw new ValidatorException("Name must be made out of letters!");
-        }
         if(book.getAuthor() == null) {
             throw new ValidatorException("author must not be null");
         }
-        if(!book.getAuthor().matches("[a-zA-Z]+")) {
+        if(!book.getAuthor().matches("[a-zA-Z. ]+")) {
             throw new ValidatorException("Author must be made out of letters!");
         }
         if(book.getPrice() < 0) {
             throw new ValidatorException("price must be greater than 0");
         }
-        if(book.getPrice() > 1000) {
+        if(book.getPrice() > 9999) {
             throw new ValidatorException("price must be greater than 0");
         }
     }
